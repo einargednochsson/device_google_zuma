aa99fc1a4a78d799298fbd00782fe03befb1a5aa
Matt Filetto <matt.filetto@gmail.com>
2023-12-02 05:04:33 -0800

    zuma: don't build ImsMediaAoc
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device.mk
+++ device.mk
@@ -897,17 +897,7 @@ endif
 
 PRODUCT_PACKAGES += ShannonRcs
 
-ifeq ($(LINEAGE_BUILD),)
-ifeq (,$(filter aosp_% factory_%,$(TARGET_PRODUCT)))
-#ImsMediaAoc library
-FEATURE_TYPE := oem_audio
-SOONG_CONFIG_NAMESPACES += audio_lib
-SOONG_CONFIG_audio_lib += \
-        audio_type
-
 SOONG_CONFIG_audio_lib_audio_type := $(FEATURE_TYPE)
-endif
-endif
 
 # ImsMedia
 PRODUCT_PACKAGES += \
