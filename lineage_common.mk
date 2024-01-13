cde6b7c1a1c79192d267b262c8b9e0b7b62af8c9
Matt Filetto <matt.filetto@gmail.com>
2024-01-13 16:44:30 -0500

    zuma: Enable adaptive charging support

--- lineage_common.mk
+++ lineage_common.mk
@@ -7,6 +7,10 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/zuma/overlay-lineage
 
+# Adaptive Charging
+PRODUCT_COPY_FILES += \
+    device/google/zuma/sysconfig/adaptivecharging.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/adaptivecharging.xml
+    
 # AiAi Config
 PRODUCT_COPY_FILES += \
     device/google/zuma/allowlist_com.google.android.as.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_com.google.android.as.xml
